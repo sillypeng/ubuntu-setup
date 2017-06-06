8ead9b24b7020a2f846f52e685604ef1a1a0490c
Zipeng WU <zipeng.wu@gmail.com>
2017-06-06 17:59:22 +0200

    update

--- init.sh
+++ init.sh
@@ -4,9 +4,9 @@ if [ "$EUID" -eq 0 ]
   exit
 fi
 
-# git
+# basic
 sudo apt update
-sudo apt install -y git gitk terminator
+sudo apt install -y build-essential libssl-dev git gitk terminator mc numlockx
 ​
 # java
 sudo add-apt-repository ppa:webupd8team/java
@@ -18,11 +18,6 @@ sudo apt install -y libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome.deb
 sudo dpkg -i /tmp/google-chrome.deb
 
-# atom
-wget -q https://github.com/atom/atom/releases/latest -O /tmp/latest
-wget -q $(awk -F '[<>]' '/href=".*atom-amd64.deb/ {match($0,"href=\"(.*.deb)\"",a); print "https://github.com/" a[1]} ' /tmp/latest) -O /tmp/atom-amd64.deb
-dpkg -i /tmp/atom-amd64.deb
-
 # spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -30,14 +25,21 @@ sudo apt update
 sudo apt install spotify-client
 
 # nodejs
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs npm build-essential
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 
 # common
 mkdir ~/worksapce
 mkdir ~/programs
 echo 'export PATH=$PATH:~/.local' >> ~/.bashrc
 echo 'export EDITOR=gedit' >> ~/.bashrc
+
+# i3wm
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
+sudo apt install -y ./keyring.deb
+sudo echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+sudo apt install -y i3
+rm ./keyring.deb
 
 # distribution upgrade
 sudo apt dist-upgrade
